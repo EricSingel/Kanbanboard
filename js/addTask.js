563d4423d07d07c57bb81cd8619c002b35f8d11b
VICENT BOTELLA <you@example.com>
2021-12-15 19:58:53 +0100

    VB: AddTask-section Anpassung

--- js/addTask.js
+++ js/addTask.js
@@ -2,10 +2,16 @@ let tasks = [];
 
 function addTask() {
   let title = document.getElementById('title').value;
+  let date = document.getElementById('date').value;
   let category = document.getElementById('category').value;
+  let urgency = document.getElementById('urgency').value;
+  let description = document.getElementById('description').value;
   let task = {
     title: title,
+    date: date,
     category: category,
+    urgency: urgency,
+    description: description,
   };
 
   tasks.push(task);
